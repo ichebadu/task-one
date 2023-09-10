9ee46d3cbea8ecfa27986fed438ed9057911293f
ichebadu <101195914+ichebadu@users.noreply.github.com>
2023-09-10 20:30:24 +0100

    task-one

--- src/main/java/com/iche/taskone/HngApplication.java
+++ src/main/java/com/iche/taskone/HngApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HngApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HngApplication.class, args);
     }
