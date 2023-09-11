61a13f2175097b533ea6d8862cc03052bf37dcdd
ichebadu <101195914+ichebadu@users.noreply.github.com>
2023-09-11 01:46:33 +0100

    task-one

--- src/main/java/com/iche/taskone/service/TaskOneService.java
+++ src/main/java/com/iche/taskone/service/TaskOneService.java
@@ -3,6 +3,5 @@ package com.iche.taskone.service;
 import com.iche.taskone.dto.response.UserResponse;
 
 public interface TaskOneService {
-
     UserResponse getUser(String slack_name, String track);
 }
